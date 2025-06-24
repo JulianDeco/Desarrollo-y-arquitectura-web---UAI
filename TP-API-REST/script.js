a4a5ad8db826442ddf985e45307a98301868e9e3
JulianDeco <juliandecoppet@gmail.com>
2025-06-23 23:12:18 -0300

    Correcion botones de paginacion y funcion de clear results

--- TP-API-REST/script.js
+++ TP-API-REST/script.js
@@ -3,13 +3,55 @@ var API_CHARACTERS = URL_API + "character"
 var API_LOCATION = URL_API + "location"
 var API_EPISODE = URL_API + "episode"
 
+var PAGES = 0
+var API_NEXT = ""
+var API_PREV = ""
 
 var div_api = document.getElementsByClassName("api")
 
-function fetch_api(destino, id = null){
+
+function clean_results_page(){
+    var results_page = document.getElementsByClassName('results-fetch')[0]
+    while (results_page.firstChild) {
+        results_page.removeChild(results_page.firstChild);
+    }
+}
+
+function buttons_pagination_api(url_prev = null, url_next = null){
+    var prev_button = document.getElementsByClassName("pagination-btn")[0]
+    var next_button = document.getElementsByClassName("pagination-btn")[1]
+
+    prev_button.setAttribute("href", url_prev);
+    prev_button.setAttribute("target", "_blank");
+
+    next_button.setAttribute("href", url_next);
+    next_button.setAttribute("target", "_blank");
+
+}
+
+function fetch_api(url){
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error en la solicitud: " + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Datos recibidos:", data);
+        buttons_pagination_api(data.info.prev, data.info.next)
+    })
+    .catch(error => {
+        console.error("Error al obtener los datos:", error)
+        alert("Ocurrió un error durante la obtención de datos:\n" + error)
+    })
+}
+
+function functions_api(destino, id = null){
     switch (destino) {
-    case "CHARACTERs":
+    case "CHARACTERS":
         console.log("El color es rojo");
+        fetch_api(API_CHARACTERS)
         break;
 
     case "SINGLE_CHARACTER":
@@ -18,13 +60,17 @@ function fetch_api(destino, id = null){
             break
         }
         var URL_SINGLE_CHARACTER = API_CHARACTERS + id
+        fetch_api(URL_SINGLE_CHARACTER)
         break;
 
     case "EPISODE":
         console.log("El color es verde");
+        fetch_api(API_EPISODE)
         break;
 
     default:
-        console.log("Color no reconocido");
+        console.log("Opción no reconocida");
 }
 }
+
+fetch_api(API_CHARACTERS)
